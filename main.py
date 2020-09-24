b867386ee146815f4ca1855710214b9a9c23caf3
Strider89 <james.provinciali@gmail.com>
2020-09-23 22:32:55 -0400

    Created function to print changes to terminal

--- main.py
+++ main.py
@@ -9,20 +9,6 @@ def create_dicts(csv_file):
             roster_data.update({row['Name'] : row['Days']})
     return roster_data
 
-def find_additions(roster1,roster2):
-    additions = []
-    for person in roster2:
-        if person not in roster1:
-            additions.append(person)
-    return additions
-
-def find_terminations(roster1,roster2):
-    terminations = []
-    for person in roster1:
-        if person not in roster2:
-            terminations.append(person)
-    return terminations
-
 #change order of roster parameters for additions and terminations
 def find_changes(roster1,roster2):
     changes = []
@@ -34,25 +20,40 @@ def find_changes(roster1,roster2):
 def find_day_changes(roster1,roster2):
     day_changes = {}
     same_keys = roster1.keys() & roster2.keys()
-    #print(same_keys)
     for name in same_keys:
-        #print(roster1.get(name))
-        #print(roster2.get(name))
         if roster1.get(name) != roster2.get(name):
-            #print(name)
-            #print(roster2.get(name))
             day_changes.update({name : roster2.get(name) })
     return day_changes
+
+
+def create_report(additions,terminations,day_changes):
+    print("The following students were added:")
+    for person in additions:
+        print(str(person) + " is new to the roster")
+    print("The following students were terminated:")
+    for person in terminations:
+        print(str(person) + " is terminated from the roster")
+    print("The follwing students changed their days:")
+    for key in day_changes:
+        if day_changes.get(key) == 'MTW':
+            person = key
+            print(str(key) + " Days changed to Monday, Tuesday, Wednesday")
+        
 
 roster1 = create_dicts('roster1.csv')
 roster2 = create_dicts('Roster2.csv')
 
-additions = find_changes(roster1,roster2)
-print(additions)
-terminations = find_changes(roster2,roster1)
-print(terminations)
+additions = find_changes(roster2,roster1)
+#print("Additions: ",additions)
+terminations = find_changes(roster1,roster2)
+#print("Terminations: ",terminations)
 day_changes = find_day_changes(roster1,roster2)
-#print(day_changes)
+#print("Day Changes", day_changes)
+
+create_report(additions,terminations,day_changes)
+
+
+
 
 
 
